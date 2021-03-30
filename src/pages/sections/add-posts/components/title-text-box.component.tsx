ffbcd7f48b9469e9b7b13639c1dafebc9aef4084
Shivam Singh <SSingh@dealersocket.com>
2021-03-30 16:32:16 +0530

    title verification integrated with error text

--- src/pages/sections/add-posts/components/title-text-box.component.tsx
+++ src/pages/sections/add-posts/components/title-text-box.component.tsx
@@ -7,41 +7,54 @@ import { InputBox } from "../../../../components/input-box/input-box.component";
 export const TitleTextBox = () => {
   const classes: any = useStyles();
   const [title, setTitle] = useState("");
-  const [isVerified, setVerification] = useState<boolean | undefined>(undefined);
-  const delayedCall = useRef(_.debounce(q => verify(q), 1000)).current;
+  const [isVerified, setVerification] = useState<boolean | undefined>(
+    undefined
+  );
+  const delayedCall = useRef(_.debounce((q) => verify(q), 1000)).current;
 
-
-  const verify = async  (title: string) => {
-    const response = await axios.get(process.env.REACT_APP_ENDPOINT_URL + '/admin/verify-title/' + title);
-    if(response.status === 200){
-      setVerification(response.data);
+  const verify = async (title: string) => {
+    if(title.trim() !== ''){
+      const response = await axios.get(
+        process.env.REACT_APP_ENDPOINT_URL + "/admin/verify-title/" + title.trim()
+      );
+      if (response.status === 200) {
+        setVerification(response.data);
+      }
     }
-  }
+    else{
+      setVerification(undefined);
+    }
+  };
   const titleChangeandler = (text: string) => {
     setTitle(text);
     delayedCall(text);
   };
   return (
-    <div className={classes.container}>
-      <InputBox
-        value={title}
-        type="text"
-        placeholder="Enter post title"
-        maxLength={500}
-        onTextChange={titleChangeandler}
-      />
-      {isVerified ? (
-        <Icon
-          className={`fas fa-check-circle ${
-            isVerified === undefined ? classes.hide : classes.showGreen
-          }`}
-        ></Icon>
-      ) : (
-        <Icon
-          className={`fas fa-times-circle ${
-            isVerified === undefined ? classes.hide : classes.showRed
-          }`}
-        ></Icon>
+    <div className={classes.content}>
+      <div className={classes.container}>
+        <InputBox
+          value={title}
+          type="text"
+          placeholder="Enter post title"
+          maxLength={500}
+          onTextChange={titleChangeandler}
+        />
+        {isVerified ? (
+          <Icon
+            className={`fas fa-check-circle ${
+              isVerified === undefined ? classes.hide : classes.showGreen
+            }`}
+          ></Icon>
+        ) : (
+          <Icon
+            className={`fas fa-times-circle ${
+              isVerified === undefined ? classes.hide : classes.showRed
+            }`}
+          ></Icon>
+        )}
+      </div>
+      {isVerified===false && (
+        <p className={classes.textDanger}>There is already a post available with the entered title.</p>
       )}
     </div>
   );
@@ -69,4 +82,11 @@ const useStyles = makeStyles(() => ({
     display: "block",
     color: "red",
   },
+  content: {
+    width: "100%",
+  },
+  textDanger: {
+    color: 'red',
+    marginTop: '-12px !important'
+  }
 }));
